d84a640050c7b3309f3b2999d8d972bcb3515447
BKUD <buuchaysatunov@gmail.com>
2025-10-31 09:01:17 +0300

    Add basic main.cpp with input/output placeholder

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by BKUD
+//
+
+#include <iostream>
+#include <string>
+
+int main() {
+    using namespace std;
+
+    string input;
+    cout << "Enter expression: ";
+    getline(cin, input);
+    cout << "You entered" << input << endl;
+}
